5eb6f281740fa764c56616681c8047fb9e34e5e4
Eduardo <eduardo.peralta12395@gmail.com>
2025-05-27 11:39:55 -0500

    grafico de streamlit

--- grafico_interactivo_churnXvar.py
+++ grafico_interactivo_churnXvar.py
@@ -27,7 +27,9 @@ print(dfParquet[columnas_deseadas].iloc[0])"""
         .astype(int)
     )
 '''
+#CREANDO MEDIAS Y OTRAS MEDIDAS
 import numpy as np
+'''
 mediaMonthly = np.mean(dfParquet['Charges.Monthly'])
 mediaTotal = np.mean(dfParquet['Charges.Total'])
 medianaMonthly = np.median(dfParquet['Charges.Monthly'])
@@ -42,3 +44,28 @@ rangoTotal =np.max(dfParquet['Charges.Total']) - np.min(dfParquet['Charges.Total
 from scipy.stats import mode
 modaMonthly = mode(dfParquet['Charges.Monthly'], keepdims=True)
 modaTotal = mode(dfParquet['Charges.Total'], keepdims=True)
+'''
+import seaborn as sns
+import matplotlib.pyplot as plt
+#GRAFICO DE CHURN
+'''
+sns.countplot(x='Churn', data=dfParquet)
+plt.title('Distribución de Clientes: Permanecen vs Se dan de baja')
+plt.xlabel('Churn')
+plt.ylabel('Cantidad de Clientes')
+plt.show()
+'''
+import streamlit as st
+import plotly.express as px
+
+categorias = ['gender','SeniorCitizen', 'Partner', 'Dependents',
+                'Contract','PaperlessBilling', 'PaymentMethod',
+                'InternetService', 'OnlineSecurity','TechSupport']
+
+st.title('Analisis de Chur(Evasion) por variables');
+seleccionarVariables = st.selectbox('Selecciona una variable categorica: ', categorias);
+
+fig = px.histogram(dfParquet, x=seleccionarVariables, color='Churn', barmode='group',
+                    title=f'Churn segun {seleccionarVariables}', text_auto=True);
+fig.update_layout(xaxis_title=seleccionarVariables, yaxis_title='Cantidad de clientes');
+st.plotly_chart(fig)
